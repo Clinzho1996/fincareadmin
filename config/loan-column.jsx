bd7d6c4a13dc435458b01466f2ad650f86a652e2
Confidence Emonena <confidinho@yahoo.com>
2025-09-16 19:00:17 +0100

    loan update

--- config/loan-column.jsx
+++ config/loan-column.jsx
@@ -430,11 +430,11 @@ const AdminLoansDashboard = () => {
 						</div>
 
 						<div className="flex flex-col md:flex-row gap-3 w-full md:w-auto">
-							<div className="relative">
-								<Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-500" />
+							<div className="flex flex-row justify-start items-center gap-4 border rounded-md px-3">
+								<Search className="left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-500" />
 								<input
 									placeholder="Search loans..."
-									className="pl-10 pr-4 py-2 border rounded-md w-full md:w-64"
+									className="pl-10 pr-4 py-2  w-full md:w-64 focus:outline-none"
 									value={searchTerm}
 									onChange={(e) => setSearchTerm(e.target.value)}
 									onKeyDown={handleSearch}
@@ -465,7 +465,7 @@ const AdminLoansDashboard = () => {
 
 							<button
 								onClick={fetchLoans}
-								className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 flex items-center gap-2">
+								className="px-4 py-2 bg-primary-1 text-white rounded-md hover:bg-blue-700 flex items-center gap-2">
 								<RefreshCw className="h-4 w-4" />
 								Refresh
 							</button>
@@ -885,7 +885,7 @@ const AdminLoansDashboard = () => {
 
 						<div className="flex justify-end border-t pt-4 mt-4 gap-2">
 							<button
-								className="px-4 py-2 border rounded-md"
+								className="px-4 py-2 border rounded-md mt-4 justify-end"
 								onClick={() => setIsDetailModalOpen(false)}>
 								Close
 							</button>
